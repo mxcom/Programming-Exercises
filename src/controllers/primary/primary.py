e2dc532f9c3e537fe14329bff0cef53a38f59c9e
ValDavSto <vdstolz@gmail.com>
2022-06-09 17:53:38 +0200

    Progress Bar added

--- src/controllers/primary/primary.py
+++ src/controllers/primary/primary.py
@@ -1,8 +1,13 @@
 from datetime import datetime
-from PySide6.QtWidgets import QMainWindow
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QMainWindow, QVBoxLayout
 from PySide6 import QtCore
 
+from src.controllers.primary.progress_bar import CircularProgress
+from src.controllers.user_management.calc_kcal import calc_kcal
 from src.views.primary.WndMain import Ui_WndMain
+from src.views.primary.uiFunctions import UIFunctions
 
 
 class PrimaryWindow(QMainWindow, Ui_WndMain):
@@ -13,4 +18,19 @@ class PrimaryWindow(QMainWindow, Ui_WndMain):
         self.ui.setupUi(self)
         self.user = user
 
+        # Create Layout
+        self.layout = QVBoxLayout()
+
+        # Create porgress bar
+        self.progress = CircularProgress(calc_kcal(
+            user.get_sex(), user.get_height(), user.get_weight(), user.get_birthday()))
+
+        self.progress.setMinimumSize(self.progress.width, self.progress.height)
+
+        # Add widgets
+        self.layout.addWidget(self.progress, Qt.AlignCenter, Qt.AlignCenter)
+
         self.ui.btnToggle.clicked.connect(lambda: UIFunctions.toggleMenu(self, 200, True))
+
+        # Set layout of the Frame
+        self.frmProgressBar.setLayout(self.layout)
