1e069391da2d2fe5ba0018ef32e65975ff26be98
ValDavSto <vdstolz@gmail.com>
2022-06-12 21:38:27 +0200

    Example bar modified

--- src/controllers/primary/stat_charts.py
+++ src/controllers/primary/stat_charts.py
@@ -1,31 +1,52 @@
-from PySide6.QtCharts import QLineSeries, QChart, QChartView, QBarSeries, QBarSet
+import datetime
+
+from PySide6.QtCharts import QLineSeries, QChart, QChartView, QBarSeries, QBarSet, QBarCategoryAxis
 from PySide6.QtGui import QPen, QColor, Qt, QPainter
 from PySide6.QtWidgets import QWidget, QVBoxLayout
+from datetime import date
+import calendar
 
 
 def create_chart():
-    #series = QListSries()
-    #series.append(0, 6)
-    #series.append(2, 4)
-    #series.append(4, 10)
-    #series.append(5, 3)
-    #series.append(6, 7)
-
+    # Create Set of Kcal for one week
     set0 = QBarSet("kcal")
     set0 << 2000 << 2400 << 1900 << 2100 << 2000 << 2350 << 1900
+    set0.setColor(QColor(0x7A64BD))
 
+    # Create Series where given set is added
     series = QBarSeries()
     series.append(set0)
     series.setBarWidth(series.count())
 
+    pen = QPen()
+    pen.setColor(QColor(0x7A64BD))
+
+    # Create Axis
+
+    curr_date = datetime.datetime.today()
+    temp_date = calendar.day_name[curr_date.weekday()]
+    categories = [temp_date[0:3]]
+    i = 0
+    for i in range(6):
+        curr_date += datetime.timedelta(days=1)
+        temp_date = calendar.day_name[curr_date.weekday()]
+        categories.append(temp_date[0:3])
+        i += 1
+
+    axisX = QBarCategoryAxis()
+    axisX.append(categories)
+    series.attachAxis(axisX)
+
+    # Create chart and set properties
     chart = QChart()
     chart.addSeries(series)
+    chart.addAxis(axisX, Qt.AlignBottom)
     chart.setAnimationOptions(QChart.SeriesAnimations)
-    chart.setTitle("Test Title")
     chart.legend().setVisible(True)
     chart.legend().setAlignment(Qt.AlignBottom)
-   
 
+
+    # Create Chart View and add created chart to chart view
     chartview = QChartView(chart)
     chartview.setRenderHint(QPainter.Antialiasing)
     return chartview
