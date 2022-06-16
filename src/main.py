e33429d113429dfd6ed25ba7b686936cd3b51572
mxcom <mxprogramming@protonmail.com>
2022-06-16 17:08:29 +0200

    add random insert for testing

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from PySide6.QtWidgets import QApplication, QMainWindow
 from src.controllers.user_management.registration import RegistrationWindow
 from src.controllers.user_management.login import LoginWindow
 from src.controllers.user_management.email import send_mail
-from src.controllers.user_management.user_management import get_user, add_user
+from src.controllers.user_management.user_management import get_user, add_user, insert_random
 from src.controllers.cryptography.cryptography import hash_passwd
 from src.controllers.user_management.calorie_management import get_stat_kcal, get_db_sys, get_db_dia
 from src.models.user_management.user import User
@@ -15,10 +15,11 @@ def main():
     # print("hello, world")
     # uncomment next line for email (and replace receiver mail)
     # send_mail('reciever@gmail.com')
-    app = QApplication(sys.argv)
-    window = LoginWindow()
-    window.show()
-    app.exec()
+    # app = QApplication(sys.argv)
+    # window = LoginWindow()
+    # window.show()
+    # app.exec()
+    insert_random()
 
 if __name__ == "__main__":
     main()
